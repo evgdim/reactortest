b3e47a3579a257c15c59ecb60efb43b8e273ecb8
Evgeni Dimitrov <evgeny.d.dimitrov@gmail.com>
2017-01-17 12:49:09 +0200

    methods for map and flatmap

--- src/main/java/com/evgeni/App.java
+++ src/main/java/com/evgeni/App.java
@@ -1,6 +1,7 @@
 package com.evgeni;
 
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.time.temporal.TemporalUnit;
@@ -13,11 +14,38 @@ public class App
 {
     public static void main( String[] args ){
         buffer();
+        flatmap();
+        flatmapMono();
+        mapVsFlatMap();
+        flatmapMonoVsMap();
     }
 
     private static void buffer() {
         Flux.fromArray(new String[]{"aa", "bb", "cc", "dd", "aaa", "bbb", "ccc", "ddd"})
                 .buffer(3)
+                .subscribe(e -> System.out.println(e));
+    }
+    private static void flatmap() {
+        Flux.fromArray(new String[]{"aa", "bb", "cc", "dd", "aaa", "bbb", "ccc", "ddd"})
+                .flatMap(str -> Flux.fromArray(str.split("")))
+                .distinct()
+                .subscribe(e -> System.out.println(e));
+    }
+    private static void flatmapMono() {
+        Flux.fromArray(new String[]{"aa", "bb", "cc", "dd", "aaa", "bbb", "ccc", "ddd"})
+                .flatMap(str -> Mono.fromSupplier(() -> str))
+                .distinct()
+                .subscribe(e -> System.out.println(e));
+    }
+    private static void mapVsFlatMap() {
+        Flux.fromArray(new String[]{"aa", "bb", "cc", "dd", "aaa", "bbb", "ccc", "ddd"})
+                .map(str -> Flux.fromArray(str.split("")))
+                .subscribe(e -> System.out.println(e));
+    }
+    private static void flatmapMonoVsMap() {
+        Flux.fromArray(new String[]{"aa", "bb", "cc", "dd", "aaa", "bbb", "ccc", "ddd"})
+                .map(str -> Mono.fromSupplier(() -> str))
+                .distinct()
                 .subscribe(e -> System.out.println(e));
     }
 }
