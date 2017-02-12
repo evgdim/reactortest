0ca98119af76a12a6ffa5d18e0ffac23266b2c68
Evgeni Dimitrov <evgeny.d.dimitrov@gmail.com>
2017-02-12 22:20:52 +0200

    mututhreaded exec

--- src/main/java/com/evgeni/App.java
+++ src/main/java/com/evgeni/App.java
@@ -1,8 +1,12 @@
 package com.evgeni;
 
+import reactor.core.publisher.BlockingSink;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
+import java.io.IOException;
 import java.time.Duration;
 import java.time.temporal.TemporalUnit;
 
@@ -12,12 +16,15 @@ import java.time.temporal.TemporalUnit;
  */
 public class App 
 {
-    public static void main( String[] args ){
-        buffer();
-        flatmap();
-        flatmapMono();
-        mapVsFlatMap();
-        flatmapMonoVsMap();
+    public static void main( String[] args ) throws IOException {
+//        buffer();
+//        flatmap();
+//        flatmapMono();
+//        mapVsFlatMap();
+//        flatmapMonoVsMap();
+        //subscribers();
+        subscribersMultyTread();
+        System.in.read();
     }
 
     private static void buffer() {
@@ -25,6 +32,7 @@ public class App
                 .buffer(3)
                 .subscribe(e -> System.out.println(e));
     }
+
     private static void flatmap() {
         Flux.fromArray(new String[]{"aa", "bb", "cc", "dd", "aaa", "bbb", "ccc", "ddd"})
                 .flatMap(str -> Flux.fromArray(str.split("")))
@@ -47,5 +55,16 @@ public class App
                 .map(str -> Mono.fromSupplier(() -> str))
                 .distinct()
                 .subscribe(e -> System.out.println(e));
+    }
+    private static void subscribers() {
+        Flux.fromArray(new String[]{"aa", "bb", "cc", "dd", "aaa", "bbb", "ccc", "ddd"})
+                .subscribeOn(Schedulers.parallel())
+                .subscribe(e -> System.out.println(e + " " + Thread.currentThread().getName()));
+    }
+
+    private static void subscribersMultyTread() {
+        Flux.fromArray(new String[]{"aa", "bb", "cc", "dd", "aaa", "bbb", "ccc", "ddd", "eee","fff","ggg", "hhh", "iii", "jjj", "kkk","aa", "bb", "cc", "dd", "aaa", "bbb", "ccc", "ddd", "eee","fff","ggg", "hhh", "iii", "jjj", "kkk"})
+                .flatMap(str -> Mono.just(str).subscribeOn(Schedulers.parallel()).map(s -> s + "_123"))
+                .subscribe(e -> System.out.println(e + " " + Thread.currentThread().getName()));
     }
 }
